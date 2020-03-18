213de57626757fb288e5b308a9487e7668838757
Igor Lins e Silva <igor@eosrio.io>
2020-03-18 02:28:03 -0300

    packages update

--- build.js
+++ build.js
@@ -1,17 +1,15 @@
-const fs = require("fs");
-const browserify = require("browserify");
-browserify(["./src/standalone.js"])
-    .transform("babelify", {
-        "presets": [
-            ["@babel/preset-env", {
-                "useBuiltIns": false,
-            }],
-        ],
-        "plugins": [
-            "@babel/plugin-proposal-class-properties",
-            "@babel/plugin-transform-modules-commonjs",
-            "@babel/plugin-transform-runtime"
-        ]
-    })
-    .bundle()
-    .pipe(fs.createWriteStream("dist/bundle.js"));
+const fs = require('fs');
+const browserify = require('browserify');
+browserify(['./src/standalone.js']).transform('babelify', {
+  'presets': [
+    [
+      '@babel/preset-env', {
+      'useBuiltIns': false,
+    }],
+  ],
+  'plugins': [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-transform-runtime',
+  ],
+}).bundle().pipe(fs.createWriteStream('dist/bundle.js'));
